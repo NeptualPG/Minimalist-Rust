e34aa2aa8e06e4f2ba3cd85a83b1f82d9fe5ae8d
NeptualPG <183859970+NeptualPG@users.noreply.github.com>
2026-09-24 19:54:09 -0500

    Remove tracked build files and ignore target folders

--- session-tracker/target/debug/deps/session_tracker.d
+++
@@ -1,9 +0,0 @@
-C:\Users\juand\Order\programming\Rust\projects\Minimalist-Rust\session-tracker\target\debug\deps\session_tracker.d: src\main.rs src\models\mod.rs src\models\session.rs src\modules\mod.rs src\modules\time_session.rs
-
-C:\Users\juand\Order\programming\Rust\projects\Minimalist-Rust\session-tracker\target\debug\deps\session_tracker.exe: src\main.rs src\models\mod.rs src\models\session.rs src\modules\mod.rs src\modules\time_session.rs
-
-src\main.rs:
-src\models\mod.rs:
-src\models\session.rs:
-src\modules\mod.rs:
-src\modules\time_session.rs:
